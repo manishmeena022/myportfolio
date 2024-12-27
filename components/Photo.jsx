004d75d0ace515f74b8c1208d94cfec2b03fd788
manishmeena022 <nsit2016manish@gmail.com>
2024-12-27 19:42:03 +0530

    changed image

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -23,7 +23,7 @@ const Photo = () => {
                     className='w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute'
                 >
                     <Image
-                        src="/assets/IMG_20190318_154726.jpg"
+                        src="/assets/userImage.png"
                         priority
                         quality={100}
                         fill
@@ -39,23 +39,23 @@ const Photo = () => {
                     viewBox="0 0 506 506"
                     xmlns="http://www.w3.org/2000/svg"
                 >
-                    <motion.circle 
-                        cx="253" 
-                        cy="253" 
-                        r="250" 
-                        stroke="#00ff99" 
-                        strokeWidth="4" 
+                    <motion.circle
+                        cx="253"
+                        cy="253"
+                        r="250"
+                        stroke="#00ff99"
+                        strokeWidth="4"
                         strokeLinecap="round"
                         strokeLinejoin="round"
-                        initial={{ strokeDasharray : "24 10 0 0"}}
+                        initial={{ strokeDasharray: "24 10 0 0" }}
                         animate={{
-                            strokeDasharray : ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
-                            rotate : [120, 360],
+                            strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+                            rotate: [120, 360],
                         }}
                         transition={{
-                            duration : 20,
-                            repeat : Infinity,
-                            repeatType : "reverse",
+                            duration: 20,
+                            repeat: Infinity,
+                            repeatType: "reverse",
                         }}
                     />
                 </motion.svg>
